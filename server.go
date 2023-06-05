6cc9de329f6558896c70dc3fecb02f281115663f
Adrian Edy <adrianedypratama@gmail.com>
2023-06-05 08:35:10 +0800

    add viper config file

--- server.go
+++ server.go
@@ -4,12 +4,25 @@ import (
 	"net/http"
 	
 	"github.com/labstack/echo/v4"
+    "github.com/spf13/viper"
 )
 
 func main() {
 	e := echo.New()
+
+	viper.SetConfigType("json")
+    viper.AddConfigPath(".")
+    viper.SetConfigName("config")
+
+    err := viper.ReadInConfig()
+    if err != nil {
+        e.Logger.Fatal(err)
+    }
+	
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	
+	e.Logger.Print("Starting", viper.GetString("appName"))
+	e.Logger.Fatal(e.Start(":" + viper.GetString("server.port")))
 }
